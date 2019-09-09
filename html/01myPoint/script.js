700155e5f32c4695f93242325d940b6c674cc7ce
IbrahimM04 <26201@ma-web.nl>
2019-09-09 11:24:09 +0200

    Introduction

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,9 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let a = new Point(new Vector2d(200, 300), 50, "rgb(50,50,50)");
+a.draw(context);
+
+let b = new Point(new Vector2d(20, 250), 10, "rgb(50,50,55)");
+b.draw(context);
