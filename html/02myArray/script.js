f563e201275f9a1a874dbdf3871402f20e564334
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2019-09-13 16:35:14 +0200

    Broken but fixed

--- html/02myArray/script.js
+++ html/02myArray/script.js
@@ -7,6 +7,9 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
+let balls = [];
+let maxBalls = 5;
+
 function rng(max) {
   return Math.floor(Math.random() * max)
 }
@@ -14,3 +17,28 @@ function rng(max) {
 function GetRandom(max) {
   return Math.random() * max;
 }
+for (let i = 0; i < maxBalls; i++) {
+  let ball = new Point(new Vector2d(rng(width), rng(height)), 20);
+  balls.push(ball);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw(context);
+  }
+}
+
+animate();
+
+window.addEventListener('click', (evt)=> {
+  let mouseVector = new Vector2d(evt.clientX,evt.clientY);
+
+  for(let i = 0; i < balls.length; i++) {
+    let distanceMouseBall = new Vector2d(0,0)
+    distanceMouseBall.differenceVector(mouseVector,balls[i].position);
+    if(distanceMouseBall.magnitude < 20) {
+      balls[i].color = "blue";
+    }
+  }
+});
