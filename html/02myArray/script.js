752859ea72f4f4ab54008c485e541a966052b993
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2019-09-13 15:33:02 +0200

    Changed some stuff

--- html/02myArray/script.js
+++ html/02myArray/script.js
@@ -14,22 +14,3 @@ function rng(max) {
 function GetRandom(max) {
   return Math.random() * max;
 }
-
-let maxBalls = 5;
-
-
-
-
-function animate() {
-  requestAnimationFrame(animate);
-  function differnceVector(a,b) {
-    a = Vector2d.dx
-  }
-  let rngWidth = rng(width);
-  let rngHeight = rng(height);
-  let point = new Point(new Vector2d(rngWidth, rngHeight), rng(10), new magnitude(rngWidth, rngHeight));
-
-  point.draw(context);
-}
-
-animate();
