f469f4ba720c1fb403aa33bad88524a77e1cc34a
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2019-09-20 14:27:36 +0200

    "finished" 03ConnectTheDots

--- html/03connectTheDots/script.js
+++ html/03connectTheDots/script.js
@@ -16,14 +16,32 @@ function rng(max) {
 for (let i = 0; i < 4; i++) {
   let ball = new Point(new Vector2d(rng(width), rng(height)), 20);
   balls.push(ball);
-  balls[i].label = i;
+  balls[i].label = i + 1;
+
 }
 
 function animate() {
   requestAnimationFrame(animate);
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw(context);
+
+    if(i == 3) {
+      context.beginPath();
+      context.moveTo(balls[3].position.dx, balls[3].position.dy);
+      context.lineTo(balls[0].position.dx, balls[0].position.dy);
+      context.strokeStyle = "black";
+      context.stroke();
+      context.closePath();
+    } else {
+      context.beginPath();
+      context.moveTo(balls[i].position.dx, balls[i].position.dy);
+      context.lineTo(balls[i + 1].position.dx, balls[i + 1].position.dy);
+      context.strokeStyle = "black";
+      context.stroke();
+      context.closePath();
+    }
   }
+
 
 }
 
