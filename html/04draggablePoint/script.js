b5199fb1b437d4cf28b9a0db939311ca9ad89973
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2019-09-28 10:14:44 +0200

    04Finish

--- html/04draggablePoint/script.js
+++ html/04draggablePoint/script.js
@@ -11,13 +11,36 @@ function rng(max) {
   return Math.floor(Math.random() * max)
 }
 
-let point = new Point(new Vector2d(500,500), 20);
+let points = [];
 
-point.label = "20";
+
 
 function animate() {
   requestAnimationFrame(animate);
-  point.draw(context);
+  context.clearRect(0, 0, width, height);
+
+  if(points.length < 4) {
+  let point = new Point(new Vector2d(rng(width),rng(height)), 20, "rgba(" + rng(255) + "," + rng(255) + "," + rng(255) + "," + 1 +")", "20", true)
+  points.push(point);
+  }
+  context.beginPath();
+  context.moveTo(points[0].position.dx, points[0].position.dy);
+
+  for (let i = 0; i < points.length; i++) {
+    context.lineTo(points[i].position.dx, points[i].position.dy);
+    context.fillStyle = "rgba(255,0,0,0.2)";
+    context.strokeStyle = "Silver";
+  }
+
+  context.closePath();
+  context.fill();
+  context.stroke();
+
+  for (let i = 0; i < points.length; i++) {
+    points[i].draw(context);
+    points[i].label = i + 1;
+  }
 }
+
 
 animate();
