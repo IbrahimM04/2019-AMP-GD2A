3933e1382e25155310d73cc9fa8c26a952fd1123
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2019-11-04 16:19:02 +0100

    12fin

--- html/12myCar/script.js
+++ html/12myCar/script.js
@@ -6,3 +6,74 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let background = new Image();
+background.src = "background.png";
+
+let frontWheel = new Image();
+frontWheel.src = "wiel.png";
+frontWheel.position = new Vector2d(0,0);
+
+let backWheel = new Image();
+backWheel.src = "wiel.png";
+backWheel.position = new Vector2d(0,0);
+
+let car = new Image();
+car.src = "auto.png";
+car.position = new Vector2d(0,0);
+car.velocity = new Vector2d(7,0);
+
+car.position.dy = height - 400;
+
+let rot = 0;
+
+addEventListener('keydown',(evt)=>{
+  switch (evt.key) {
+    case "ArrowRight":
+      car.velocity.dx += 1;
+      break;
+      case "ArrowLeft":
+      car.velocity.dx -=1;
+      break;
+    default:
+  }
+})
+
+function animate() {
+  requestAnimationFrame(animate);
+  context.clearRect(0,0,width,height);
+  car.position.add(car.velocity);
+  context.drawImage(car, car.position.dx, car.position.dy);
+
+  context.save();
+  context.translate(car.position.dx+207, car.position.dy+193);
+  if (car.velocity.dx != 0) {
+    context.rotate(rot);
+  }
+  context.drawImage(frontWheel,-frontWheel.width/2,-frontWheel.height/2);
+  context.restore();
+
+  context.save();
+  context.translate(car.position.dx+727.5, car.position.dy+193);
+  if (car.velocity.dx != 0) {
+    context.rotate(rot);
+  }
+  context.drawImage(backWheel,-frontWheel.width/2,-frontWheel.height/2);
+  context.restore();
+
+  clamp();
+
+  rot += car.velocity.dx/80
+
+
+}
+
+animate();
+
+
+
+function clamp() {
+  if (car.position.dx > width) {
+    car.position.dx = -car.width;
+  }
+}
