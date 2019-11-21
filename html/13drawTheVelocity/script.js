68ff7ac924fbcc0fd89adc9af462b745eb0be71e
IbrahimM04 <43440821+IbrahimM04@users.noreply.github.com>
2019-11-21 11:20:39 +0100

    13

--- html/13drawTheVelocity/script.js
+++ html/13drawTheVelocity/script.js
@@ -7,12 +7,14 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let point = new DPoint(new Vector2d(200,200),new Vector2d(3,4), new Vector2d(0,0), "purple", A);
+let point = new DPoint(new Vector2d(0,0),new Vector2d(3,4), new Vector2d(0,0), 20, "purple");
 
 function animate() {
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
   point.draw(context);
+  point.update();
+  point.vel.draw(context,point.pos, "red",5);
 }
 
 animate();
